42d4401c2daba62e70c2eb9a98588f1f808e347a
ICE-sherbet <ice.chiruno9@gmail.com>
2022-08-08 09:40:09 +0900

    CollisionLayer定義
    ElectricsのSender,Receiverの定義

---
+++ Project/CollisionLayer.h
@@ -0,0 +1,65 @@
+﻿// @file CollisionLayer.h
+// @brief
+// @author ICE
+// @date 2022/08/08
+//
+// @details
+
+#pragma once
+#include "CollisionComponent.h"
+
+namespace detail {
+constexpr unsigned long long Shift(size_t val) { return 1 << val; }
+}  // namespace detail
+
+class CollisionLayer final {
+ public:
+  // kBeaconTargetFilter = kSenderFilter
+  enum Layer {
+    kPlayerFilter = 1 << 1,
+    kMachineFilter = 1 << 4,
+    kReceiveFilter = 1 << 5,
+    kSenderFilter = 1 << 6,
+
+    kElectricityAreaFilter = 1 << 8,
+
+  };
+
+ private:
+  using U = typename std::underlying_type<Layer>::type;
+
+ public:
+  constexpr CollisionLayer(const U val) noexcept
+      : val_(static_cast<Layer>(val)) {}
+
+  constexpr operator U() const { return val_; }
+
+ private:
+  Layer val_;
+};
+using BitCollisionLayer = std::bitset<base_engine::kCollisionFilterSize>;
+constexpr BitCollisionLayer kBeaconObjectFilter{CollisionLayer::kMachineFilter |
+                                                CollisionLayer::kReceiveFilter |
+                                                CollisionLayer::kSenderFilter};
+
+constexpr BitCollisionLayer kBeaconTargetFilter{CollisionLayer::kMachineFilter |
+                                                CollisionLayer::kReceiveFilter};
+/*
+using BitCollisionLayer = std::bitset<base_engine::kCollisionFilterSize>;
+#define SET_DEFINITION_LAYER(layer_name, val) \
+  constexpr size_t layer_name##Number = val;  \
+  constexpr CollisionLayer layer_name{detail::Shift(kPlayerFilterNumber)};
+
+#define GET_LAYER(layer_name) layer_name##Number
+#define SET_MULTI_LAYER(layer_name, val) \
+  constexpr CollisionLayer layer_name{val};
+
+SET_DEFINITION_LAYER(kPlayerFilter, 0)
+SET_DEFINITION_LAYER(kMachineFilter, 4)
+SET_DEFINITION_LAYER(kReceiverFilter, 6)
+SET_DEFINITION_LAYER(kElectricityAreaFilter, 8)
+
+SET_MULTI_LAYER(kBeaconObjectFilter, GET_LAYER(kMachineFilter))
+constexpr CollisionLayer kBeaconObjectFilter =
+    (kMachineFilter | kReceiverFilter);
+*/
--- Project/ISendablePower.h
+++ Project/ISendablePower.h
@@ -1,7 +1,15 @@
 #pragma once
 #include <vector>
-
+template <typename T>
+concept SendablePower = requires(T& t) {
+  {
+    t.GetTarget()
+    } -> std::convertible_to<std::vector<class IReceivablePower*>>;
+  t.Sending();
+};
 class ISendablePower {
-  std::vector< class IReceivablePower* > virtual GetTarget() = 0;
+public:
+    virtual ~ISendablePower() = default;
+    std::vector<class IReceivablePower*> virtual GetTarget() = 0;
   void virtual Sending() = 0;
 };
