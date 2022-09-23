f52d28be92818bb59d5746a6e297f496d950de1d
ICE-sherbet <ice.chiruno9@gmail.com>
2022-09-23 20:17:04 +0900

    RenderCommand

--- Project/RenderMof.cpp
+++ Project/RenderMof.cpp
@@ -1,7 +1,55 @@
 ﻿#include "RenderMof.h"
-namespace base_engine 
-{
-RenderMof::RenderMof(){
 
+#include <Utilities/GraphicsUtilities.h>
+using Mof::CGraphicsUtilities;
+namespace base_engine {
+RenderMof::RenderMof() {}
+
+void RenderMof::AddTexture(const ITexturePtr texture, const Vector& position,
+                           const Vector& scale, float angle, const Rect& uv,
+                           const Color& color,
+                           Mof::TextureAlignment alignment) {
+  CGraphicsUtilities::RenderScaleRotateTexture(
+      position.x + camera_position_.x, position.y + camera_position_.y, scale.x,
+      scale.y, angle, uv, color, alignment, texture);
 }
+
+void RenderMof::AddLine(const Vector& position1, const Vector& position2,
+                        const Color& color) {
+  CGraphicsUtilities::RenderLine(position1.x + camera_position_.x,
+                                 position1.y + camera_position_.y,
+                                 position2.x + camera_position_.x,
+                                 position2.y + camera_position_.y, color);
 }
+
+void RenderMof::AddRect(const Rect& rect, const Color& color) {
+  CGraphicsUtilities::RenderFillRect(
+      rect.Left + camera_position_.x, rect.Top + camera_position_.y,
+      rect.Right + camera_position_.x, rect.Bottom + camera_position_.y, color);
+}
+
+void RenderMof::AddRectFrame(const Rect& rect, const Color& color) {
+  CGraphicsUtilities::RenderRect(
+      rect.Left + camera_position_.x, rect.Top + camera_position_.y,
+      rect.Right + camera_position_.x, rect.Bottom + camera_position_.y, color);
+}
+
+void RenderMof::AddCircle(const Circle& circle, const Color& color) {
+  CGraphicsUtilities::RenderFillCircle(circle.Position.x + camera_position_.x,
+                                       circle.Position.y + camera_position_.y,
+                                       circle.r, color);
+}
+
+void RenderMof::AddCircleFrame(const Circle& circle, const Color& color) {
+  CGraphicsUtilities::RenderCircle(circle.Position.x + camera_position_.x,
+                                   circle.Position.y + camera_position_.y,
+                                   circle.r, color);
+}
+
+void RenderMof::SetCameraPosition(const Vector& position) {
+  camera_position_ = position;
+  const auto& graphics = CGraphicsUtilities::GetGraphics();
+  camera_position_.x += graphics->GetTargetWidth() / static_cast<float>(2);
+  camera_position_.y += graphics->GetTargetHeight() / static_cast<float>(2);
+}
+}  // namespace base_engine
--- Project/RenderMof.h
+++ Project/RenderMof.h
@@ -1,9 +1,27 @@
 ﻿#pragma once
+
 #include "IBaseEngineRender.h"
 namespace base_engine {
 class RenderMof final : public IBaseEngineRender {
+  Vector camera_position_{0,0};
+
  public:
   RenderMof();
+  void AddTexture(ITexturePtr texture, const Vector& position,
+                  const Vector& scale, float angle, const Rect& uv,
+                  const Color& color, Mof::TextureAlignment alignment) override;
+  void AddLine(const Vector& position1, const Vector& position2,
+               const Color& color) override;
+  void AddRect(const Rect& rect, const Color& color) override;
+
+  void AddRectFrame(const Rect& rect, const Color& color) override;
+  void AddCircle(const Circle& circle, const Color& color) override;
+
+  void AddCircleFrame(const Circle& circle, const Color& color) override;
+  void SetCameraPosition(const Vector& position) override;
+  Vector GetCameraPosition() override
+  { return camera_position_;
+  }
 private:
 };
 }  // namespace base_engine
