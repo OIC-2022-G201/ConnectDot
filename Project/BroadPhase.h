b8be016ab38a3a4ae1b95bae92c019df8ec1d349
ICE <ice.chiruno9@gmail.com>
2022-10-21 17:21:39 +0900

    marge error fix

--- Project/BroadPhase.h
+++ Project/BroadPhase.h
@@ -22,7 +22,19 @@ concept HasAddPairs = requires(T& x, void* a, void* b) {
 };
 class BroadPhase {
  public:
-  BroadPhase();
+  BroadPhase() {
+    m_proxyCount = 0;
+
+    m_pairCapacity = 16;
+    m_pairCount = 0;
+    m_pairBuffer =
+        static_cast<b2Pair*>(malloc(m_pairCapacity * sizeof(b2Pair)));
+
+    m_moveCapacity = 16;
+    m_moveCount = 0;
+    m_moveBuffer =
+        static_cast<int32_t*>(malloc(m_moveCapacity * sizeof(int32_t)));
+  }
 
   ~BroadPhase() {
     free(m_moveBuffer);
