3cc50cd157d5ff4f1d4a8f93e660f7658785636b
ICE <ice.chiruno9@gmail.com>
2022-12-02 08:15:45 +0900

    Door Component Create

---
+++ Project/DoorComponent.cpp
@@ -0,0 +1,1 @@
+﻿#include "DoorComponent.h"
---
+++ Project/DoorComponent.h
@@ -0,0 +1,14 @@
+﻿// @DoorComponent.h
+// @brief
+// @author ICE
+// @date 2022/12/02
+// 
+// @details
+
+#pragma once
+
+class DoorComponent
+{
+public:
+    
+};
