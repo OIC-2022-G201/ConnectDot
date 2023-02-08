72a554e292d63606e0aec9cb8ab1e7633f197e35
ICE <ice.chiruno9@gmail.com>
2023-02-08 21:17:52 +0900

    デストラクタ時にストップをかける

--- Project/AudioStreamComponent.cpp
+++ Project/AudioStreamComponent.cpp
@@ -1,1 +1,31 @@
 ﻿#include "AudioStreamComponent.h"
+
+#include "ResourceContainer.h"
+base_engine::AudioStreamComponent::AudioStreamComponent(Actor* owner): Component(owner)
+{}
+
+base_engine::AudioStreamComponent::~AudioStreamComponent()
+{ Stop(); }
+
+void base_engine::AudioStreamComponent::SetAudioBuffer(const MofSoundBufferPtr audio)
+{ audio_buffer_ = audio; }
+
+bool base_engine::AudioStreamComponent::AssetLoad(const std::string& name)
+{
+	audio_buffer_ =
+		ResourceContainer::GetResource<ResourceContainer::SoundResourcePack,
+		                               ResourceContainer::Sound>(name);
+	return audio_buffer_ != nullptr;
+}
+
+void base_engine::AudioStreamComponent::Play() const
+{ audio_buffer_->Play(); }
+
+void base_engine::AudioStreamComponent::Stop() const
+{ audio_buffer_->Stop(); }
+
+void base_engine::AudioStreamComponent::Resume() const
+{ audio_buffer_->Resume(); }
+
+void base_engine::AudioStreamComponent::SetVolume(const float volume)
+{ audio_buffer_->SetVolume(volume); }
--- Project/AudioStreamComponent.h
+++ Project/AudioStreamComponent.h
@@ -6,40 +6,22 @@
 // @details
 
 #pragma once
-#include <Sound/XAudio/XAudioSoundBuffer.h>
-
 #include "Component.h"
 #include "ResourceContainer.h"
-
 namespace base_engine {
-struct Del {
-  constexpr Del() noexcept = default;
-
-  void operator()(Mof::ISoundBuffer* _Ptr) const noexcept {
-    static_assert(0 < sizeof(Mof::ISoundBuffer),
-                  "can't delete an incomplete type");
-    _Ptr->Release();
-    delete _Ptr;
-  }
-};
-class AudioStreamComponent : public Component {
+class AudioStreamComponent final : public Component {
   using MofSoundBufferPtr = std::shared_ptr<ResourceContainer::Sound>;
   MofSoundBufferPtr audio_buffer_ = nullptr;
 
  public:
-  explicit AudioStreamComponent(Actor* owner) : Component(owner) {}
-  void SetAudioBuffer(const MofSoundBufferPtr audio) { audio_buffer_ = audio; }
+  explicit AudioStreamComponent(Actor* owner);
+  ~AudioStreamComponent() override;
+  void SetAudioBuffer(const MofSoundBufferPtr audio);
+  bool AssetLoad(const std::string& name);
 
-  bool AssetLoad(const std::string& name) {
-    audio_buffer_ =
-        ResourceContainer::GetResource<ResourceContainer::SoundResourcePack,
-                                       ResourceContainer::Sound>(name);
-    return audio_buffer_ != nullptr;
-  }
-
-  void Play() const { audio_buffer_->Play(); }
-  void Stop() const { audio_buffer_->Stop(); }
-  void Resume() const { audio_buffer_->Resume(); }
-  void SetVolume(const float volume) { audio_buffer_->SetVolume(volume); }
+  void Play() const;
+  void Stop() const;
+  void Resume() const;
+  void SetVolume(const float volume);
 };
 }  // namespace base_engine
