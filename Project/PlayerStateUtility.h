9339ff39a2f6261fea3e361743ebeb3d61d95ece
0v0d <w>
2023-02-14 18:24:51 +0900

    CalculationJumpAccelerationの数式の修正

--- Project/PlayerStateUtility.h
+++ Project/PlayerStateUtility.h
@@ -6,5 +6,5 @@ using std::numbers::pi_v;
 static float CalculationJumpAcceleration(const float H, const uint16_t T, const uint16_t X){
 	const float t = T;
 	const float x = X;
-	return  (H / t) / t * std::sin(pi_v<float>*x / t * -1) * std::pow(pi_v<float>, 2);
+	return (H / t) / t * -std::sin(pi_v<float>*x / t) * std::pow(pi_v<float>, 2);
 }
