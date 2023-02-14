290bb1e589947cb2102df1a27dad6d1202c5dc74
hasegawa2003@outlook.jp <hasegawa2003@outlook.jp>
2023-02-15 01:11:20 +0900

    挙動を修正。カメラが矩形外の対象を追うようになった。

--- Project/CameraCustomComponent.cpp
+++ Project/CameraCustomComponent.cpp
@@ -6,24 +6,22 @@ CameraCustomComponent::CameraCustomComponent(base_engine::Actor* owner):Componen
 
 void CameraCustomComponent::Update() {
 	if (GetOwner().expired()) return;
-
-	const auto actor = actor_weak_.lock();
-	const auto actor_pos = inside_camera_room_ ? target_actor_pos_ : actor->GetPosition() + offset_;
-	MoveCamera(actor_pos);
+	MoveCamera();
 }
 
-void CameraCustomComponent::MoveCamera(base_engine::Vector2 target_pos) {
-
+void CameraCustomComponent::MoveCamera() {
+	const auto actor = actor_weak_.lock();
+	const auto actor_pos = actor->GetPosition() + offset_;
 	base_engine::Vector2 current_pos = owner_->GetPosition();
-	base_engine::Vector2 targetDirection = (target_pos - current_pos);
+	targetDirection = (actor_pos - current_pos);
 
-	inside_camera_room_ = camera_room_.CollisionPoint(actor_weak_.lock()->GetPosition());
-	if (inside_camera_room_) {
-		target_actor_pos_ = target_pos;
-	}
+	bool inside_x = (targetDirection.x > camera_window_.Left && targetDirection.x < camera_window_.Right);
+	bool inside_y = (targetDirection.y > camera_window_.Top && targetDirection.y < camera_window_.Bottom);
 
-	float interp_velocity =
-		base_engine::VectorUtilities::Length(targetDirection) * 5.0f;
+	if (inside_x && inside_y) return;
+
+	const float interp_velocity =
+		base_engine::VectorUtilities::Length(CalcuDirection(inside_x, inside_y)) * 5.0f;
 
 	target_pos_ =
 		current_pos + (base_engine::VectorUtilities::Normalize(targetDirection) *
@@ -34,12 +32,33 @@ void CameraCustomComponent::MoveCamera(base_engine::Vector2 target_pos) {
 		std::lerp(current_pos.y, target_pos_.y,
 				  current_pos.y > target_pos_.y ? 0.2f : 0.6f) };
 
-	if (!camera_window_.CollisionPoint(move_pos)) {
-		if (move_pos.x < camera_window_.Left) move_pos.x = camera_window_.Left;
-		else if (move_pos.x > camera_window_.Right) move_pos.x = camera_window_.Right;
-		if (move_pos.y < camera_window_.Top) move_pos.y = camera_window_.Top;
-		else if (move_pos.y > camera_window_.Bottom) move_pos.y = camera_window_.Bottom;
+	if (!camera_room_.CollisionPoint(move_pos)) {
+		if (move_pos.x < camera_room_.Left) move_pos.x = camera_room_.Left;
+		else if (move_pos.x > camera_room_.Right) move_pos.x = camera_room_.Right;
+		if (move_pos.y < camera_room_.Top) move_pos.y = camera_room_.Top;
+		else if (move_pos.y > camera_room_.Bottom) move_pos.y = camera_room_.Bottom;
 	}
 
 	owner_->SetPosition(move_pos);
+}
+
+base_engine::Vector2 CameraCustomComponent::CalcuDirection(bool inside_x, bool inside_y) {
+	base_engine::Vector2 dir = base_engine::Vector2(0, 0);
+	if (!inside_x) {
+		if (targetDirection.x < camera_window_.Left) {
+			dir.x = targetDirection.x - camera_window_.Left;
+		}
+		else if(targetDirection.x > camera_window_.Right){
+			dir.x = targetDirection.x - camera_window_.Right;
+		}
+	}
+	if (!inside_y) {
+		if (targetDirection.y < camera_window_.Top) {
+			dir.y = targetDirection.y - camera_window_.Top;
+		}
+		else if (targetDirection.x > camera_window_.Bottom) {
+			dir.y = targetDirection.y - camera_window_.Bottom;
+		}
+	}
+	return dir;
 }
